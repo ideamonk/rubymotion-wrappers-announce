3b15c19f231b3459bfed241f8a82e2065aa03444
Abhishek Mishra <ideamonk@gmail.com>
2012-09-13 21:48:36 +0530

    testing env at heroku

--- announce.rb
+++ announce.rb
@@ -1,5 +1,15 @@
-require 'sinatra'
+#require 'sinatra'
+require 'twitter_oauth'
+
+client = TwitterOAuth::Client.new(
+  :consumer_key    => ENV['TWITTER_CONSUMER_KEY'],
+  :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'],
+  :token           => ENV['TWITTER_ACCESS_TOKEN'],
+  :secret          => ENV['TWITTER_ACCESS_SECRET']
+)
+
+client.update("Hola from Heroku")
 
 get '/' do
-  "Hello World!"
+  "Hi there! I announce new new new Rubymotion Wrappers. Follow me at <a href='https://twitter.com/RM_Wrappers'>@RM_Wrappers</a>"
 end
